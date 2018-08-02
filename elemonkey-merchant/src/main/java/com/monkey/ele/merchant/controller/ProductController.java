b4408c57a346dfeee11b26a968ae6362b17c79ce
CAIKE3 <495725770@qq.com>
2018-08-02 19:09:03 +0800

    update ProductController

--- elemonkey-merchant/src/main/java/com/monkey/ele/merchant/controller/ProductController.java
+++ elemonkey-merchant/src/main/java/com/monkey/ele/merchant/controller/ProductController.java
@@ -1,5 +1,8 @@
 package com.monkey.ele.merchant.controller;
 
+import com.monkey.ele.common.pojo.MessageResultCode;
+import com.monkey.ele.common.pojo.ResponseMessage;
+import com.monkey.ele.merchant.pojo.Message;
 import com.monkey.ele.merchant.pojo.Product;
 import com.monkey.ele.merchant.pojo.User;
 import com.monkey.ele.merchant.service.ProductService;
@@ -17,30 +20,45 @@ public class ProductController {
 
 
     @PostMapping
-    public Product addProduct(@RequestBody Product product){
-        return productService.saveProduct(product);
+    public ResponseMessage addProduct(@RequestBody Product product){
+        Product saveProduct = productService.saveProduct(product);
+        ResponseMessage message = null;
+        if(saveProduct == null){
+            message = new ResponseMessage(null,MessageResultCode.ERROR,Message.MSG_ADD_ERROR);
+        }else{
+            message = new ResponseMessage(saveProduct,MessageResultCode.SUCCESS,Message.MSG_ADD_SUCCESS);
+        }
+        return message;
     }
 
     @DeleteMapping(value = "{id}")
-    public void deleteProduct(@PathVariable String id){
+    public ResponseMessage deleteProduct(@PathVariable String id){
         productService.deleteProduct(id);
+        return new ResponseMessage(null,MessageResultCode.SUCCESS,Message.MSG_DELETE_SUCCESS);
     }
 
 
     @PutMapping
-    public Product updateProduct(Product product){
-        return productService.updateProduct(product);
+    public ResponseMessage updateProduct(@RequestBody Product product){
+        Product updateProduct = productService.updateProduct(product);
+        ResponseMessage message = null;
+        if(updateProduct == null){
+            message = new ResponseMessage(null,MessageResultCode.ERROR,Message.MSG_UPDATE_ERROR);
+        }else{
+            message = new ResponseMessage(updateProduct,MessageResultCode.SUCCESS,Message.MSG_UPDATE_SUCCESS);
+        }
+        return message;
     }
 
 
     @GetMapping
-    public List<Product> findAllProducts(){
-        return productService.findAllProducts();
+    public ResponseMessage findAllProducts(){
+        return new ResponseMessage(productService.findAllProducts(),MessageResultCode.SUCCESS,null);
     }
 
     @GetMapping(value = "{id}")
-    public Product loadProduct(@PathVariable String id){
-        return productService.loadProduct(id);
+    public ResponseMessage loadProduct(@PathVariable String id){
+        return new ResponseMessage(productService.loadProduct(id),MessageResultCode.SUCCESS,null);
     }
 
 
