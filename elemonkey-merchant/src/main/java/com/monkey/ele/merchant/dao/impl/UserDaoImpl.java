73c166ee2cc0d13222c584bed1356b1e28871002
CAIKE3 <495725770@qq.com>
2018-08-02 10:30:39 +0800

    update UserDaoImpl

--- elemonkey-merchant/src/main/java/com/monkey/ele/merchant/dao/impl/UserDaoImpl.java
+++ elemonkey-merchant/src/main/java/com/monkey/ele/merchant/dao/impl/UserDaoImpl.java
@@ -3,6 +3,8 @@ package com.monkey.ele.merchant.dao.impl;
 import com.monkey.ele.common.dao.impl.AbstractBaseDao;
 import com.monkey.ele.merchant.dao.UserDao;
 import com.monkey.ele.merchant.pojo.User;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class UserDaoImpl extends AbstractBaseDao<User> implements UserDao {
 }
