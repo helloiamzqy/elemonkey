63f1409357e697a674ee0908a2eb95151ac7d95e
libu <burgess.li@oocl.com>
2018-08-02 14:20:09 +0800

    update customer dao

--- elemonkey-customer/src/main/java/com/monkey/ele/customer/dao/UserDao.java
+++ elemonkey-customer/src/main/java/com/monkey/ele/customer/dao/UserDao.java
@@ -1,12 +1,12 @@
 package com.monkey.ele.customer.dao;
 
-import org.springframework.stereotype.Repository;
-
+import com.monkey.ele.common.dao.BaseDao;
+import com.monkey.ele.customer.pojo.User;
 /**
  * @author Burgess Li
  * @version 1.0
  * @date 8/2/2018 2:14 PM
  **/
-@Repository
-public class UserDao {
+public interface UserDao extends BaseDao<User> {
+
 }
