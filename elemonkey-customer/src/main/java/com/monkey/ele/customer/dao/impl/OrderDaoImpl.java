d104b72499d963521665a473440c464ee89d5fe6
CAIKE3 <495725770@qq.com>
2018-08-03 14:10:28 +0800

    update OrderDao and ProductDao add order by createTime desc

--- elemonkey-customer/src/main/java/com/monkey/ele/customer/dao/impl/OrderDaoImpl.java
+++ elemonkey-customer/src/main/java/com/monkey/ele/customer/dao/impl/OrderDaoImpl.java
@@ -5,6 +5,19 @@ import com.monkey.ele.customer.dao.OrderDao;
 import com.monkey.ele.customer.pojo.Order;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public class OrderDaoImpl extends AbstractBaseDao<Order> implements OrderDao {
+    @Override
+    public List<Order> findHistoryOrder(String uid) {
+        List<Order> orders = this.find("SELECT o FROM Order o WHERE o.status in (3,4,5) and o.userId = ? order by o.createTime desc", uid);
+        return orders;
+    }
+
+    @Override
+    public List<Order> findActiveOrder(String uid) {
+        List<Order> orders = this.find("SELECT o FROM Order o WHERE o.status in (0,1,2) and o.userId = ? order by o.createTime desc", uid);
+        return orders;
+    }
 }
