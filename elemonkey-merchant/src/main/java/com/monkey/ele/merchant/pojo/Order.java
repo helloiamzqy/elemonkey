608fc62cd45b7f425b74404db53f0c97426db494
Lance Li <lance.li@oocl.com>
2018-08-04 18:12:00 +0800

    update M

--- elemonkey-merchant/src/main/java/com/monkey/ele/merchant/pojo/Order.java
+++ elemonkey-merchant/src/main/java/com/monkey/ele/merchant/pojo/Order.java
@@ -58,7 +58,7 @@ public class Order {
     private Integer status;
     @Column(updatable = false)
     @CreationTimestamp
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone="GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm", timezone="GMT+8")
     @Temporal(TemporalType.TIMESTAMP)
     private Date createTime;
 
