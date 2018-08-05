24a3d44b83789c2dc9c859f509fcfaa3a7a967fd
ChenYi1996 <717572168@qq.com>
2018-08-05 19:00:05 +0800

    add pwdManager

--- elemonkey-customer/src/main/java/com/monkey/ele/customer/service/UserService.java
+++ elemonkey-customer/src/main/java/com/monkey/ele/customer/service/UserService.java
@@ -11,5 +11,7 @@ public interface UserService {
 
     User findUserByUsername(String username);
     User save(User user);
+    User findUserById(String userId);
+    User updateUser(User user);
 
 }
